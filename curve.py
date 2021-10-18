b869d1c8820a0936e0df1981399483ffb6f054d5
Viktor Markovec <markvik42@gmail.com>
2021-10-18 12:37:11 +0300

    feat(curve): add curve methods and some fixs

---
+++ curve.py
@@ -0,0 +1,101 @@
+from dot import Dot
+import numpy as np
+
+
+def sum_matrix(left: np.ndarray, right: np.ndarray) -> np.ndarray:
+    if left.shape[0] != right.shape[0] and left.shape[1] != right.shape[1]:
+        raise Exception('can\'t sum matrix')
+    res = np.zeros(left.shape)
+    for i in range(len(left)):
+        a = left[i]
+        b = right[i]
+        c = res[i]
+        for j in range(len(a)):
+            c[j] = a[j] + b[j]
+
+
+def mult_matrix(left: np.ndarray, right: np.ndarray) -> np.ndarray:
+    res = np.zeros((left.shape[0], right.shape[1]))
+    if (left.shape[1] != right.shape[0]):
+        raise Exception('can\'t mult matrix')
+    for i in range(left.shape[0]):
+        for j in range(right.shape[1]):
+            for k in range(right.shape[0]): 
+                res[i][j] += left[i][k] * right[k][j]
+    return res
+
+
+def transpose_matrix(matrix: np.ndarray) -> np.ndarray:
+    res = np.zeros(matrix.shape)
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            res[i][j] = matrix[j][i]
+    return res
+
+
+def hermite_shape(dots) -> list:
+    res_dots = []
+    hermite_matrix = np.array([
+        [2, -2, 1, 1],
+        [-3, 3, -2, -1],
+        [0, 0, 1, 0],
+        [1, 0, 0, 0]
+    ])
+    dots_matrix = np.array([
+        [dots[0].x, dots[0].y],
+        [dots[1].x, dots[1].y],
+        [dots[2].x, dots[2].y],
+        [dots[3].x, dots[3].y]
+    ])
+    coefficient = mult_matrix(hermite_matrix, dots_matrix)
+    for t in range(101):
+        t_matrix = np.array([[pow(t/100,3), pow(t/100,2), t/100, 1]])
+        dot_matrix = mult_matrix(t_matrix, coefficient)
+        res_dots.append(Dot(x = dot_matrix[0][0], y = dot_matrix[0][1]))
+    return res_dots
+
+
+def bezier_shape(dots) -> list:
+    # ((x_3 - x_1) * (y_2 - y_1)) == (y_3 - y_1) * (x_2 - x_1)
+    res_dots = []
+    hermite_matrix = np.array([
+        [-1, 3, -3, 1],
+        [3, -6, 3, 0],
+        [-3, 3, 0, 0],
+        [1, 0, 0, 0]
+    ])
+    dots_matrix = np.array([
+        [dots[0].x, dots[0].y],
+        [dots[1].x, dots[1].y],
+        [dots[2].x, dots[2].y],
+        [dots[3].x, dots[3].y]
+    ])
+    coefficient = mult_matrix(hermite_matrix, dots_matrix)
+    for t in range(101):
+        t_matrix = np.array([[pow(t/100,3), pow(t/100,2), t/100, 1]])
+        dot_matrix = mult_matrix(t_matrix, coefficient)
+        res_dots.append(Dot(x = dot_matrix[0][0], y = dot_matrix[0][1]))
+    return res_dots
+
+
+def b_spline(dots: list) -> list:
+    res_dots = []
+    hermite_matrix = np.array([
+        [-1, 3, -3, 1],
+        [3, -6, 3, 0],
+        [-3, 0, 3, 0],
+        [1, 4, 1, 0]
+    ])
+    for i in range(1, len(dots)-2):
+        dots_matrix = np.array([
+            [dots[i-1].x, dots[i-1].y],
+            [dots[i].x, dots[i].y],
+            [dots[i+1].x, dots[i+1].y],
+            [dots[i+2].x, dots[i+2].y]
+        ])
+        coefficient = mult_matrix(hermite_matrix, dots_matrix)
+        for t in range(11):
+            t_matrix = np.array([[pow(t/10,3)/6, pow(t/10,2)/6, t/10/6, 1/6]])
+            dot_matrix = mult_matrix(t_matrix, coefficient)
+            res_dots.append(Dot(x = dot_matrix[0][0], y = dot_matrix[0][1]))
+    return res_dots
--- ui.py
+++ ui.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
-import line
-import second_order_lines
+
 import time
 from enum import Enum
+from tkinter.constants import DISABLED
+
+import line
+import second_order_lines
+import curve
 
 
 color_intesity = {
@@ -26,8 +30,11 @@ class App(tk.Tk):
         super().__init__()
 
         self.lines = []
+        self.curves = []
+        self.dots = []
         self.begin = None
-        self.dots = None
+        self.obj = None
+        self.is_multiple_dots = False
         self.debug = tk.BooleanVar(False)
         self.draw_method = line.digital_differential_analyzer
 
@@ -51,9 +58,15 @@ class App(tk.Tk):
         second_order_lines_menu.add_command(label='Гипербола', command=self.hyperbola)
         second_order_lines_menu.add_command(label='Парабола', command=self.parabola)
 
+        curve_menu = tk.Menu(self.menu, tearoff=0)
+        curve_menu.add_command(label='Эрмит', command=self.hermite)
+        curve_menu.add_command(label='Безье', command=self.bezier)
+        curve_menu.add_command(label='В сплайн', command=self.b_spline)
+
         self.menu.add_cascade(label='Файл', menu = file_menu)
         self.menu.add_cascade(label='Отрезки', menu = line_menu)
         self.menu.add_cascade(label='Линии второго порядка', menu = second_order_lines_menu)
+        self.menu.add_cascade(label='Кривые', menu = curve_menu)
 
         self.toolbar = tk.Frame(self.master, bd=1, relief=tk.RAISED)
         
@@ -75,8 +88,8 @@ class App(tk.Tk):
         )
         wu_button.pack(side=tk.LEFT)
 
-        sep = ttk.Separator(self.toolbar)
-        sep.pack(side=tk.LEFT, fill=tk.Y)
+        sep1 = ttk.Separator(self.toolbar)
+        sep1.pack(side=tk.LEFT, fill=tk.Y)
 
         circle_button = tk.Button(
             self.toolbar, relief=tk.FLAT,
@@ -102,6 +115,27 @@ class App(tk.Tk):
         )
         parabola_button.pack(side=tk.LEFT)
 
+        sep2 = ttk.Separator(self.toolbar)
+        sep2.pack(side=tk.LEFT, fill=tk.Y)
+
+        hermite_button = tk.Button(
+            self.toolbar, relief=tk.FLAT,
+            command=self.hermite, text='Эрмит'
+        )
+        hermite_button.pack(side=tk.LEFT)
+
+        bezier_button = tk.Button(
+            self.toolbar, relief=tk.FLAT,
+            command=self.bezier, text='Безье'
+        )
+        bezier_button.pack(side=tk.LEFT)
+
+        b_spline_button = tk.Button(
+            self.toolbar, relief=tk.FLAT,
+            command=self.b_spline, text='B сплайн'
+        )
+        b_spline_button.pack(side=tk.LEFT)
+
         self.toolbar.pack(side=tk.TOP, fill=tk.X)
 
         self.width = 1920
@@ -110,90 +144,189 @@ class App(tk.Tk):
 
         self.canvas.bind("<Button-1>", self.mouse_pressed)
         self.canvas.bind("<Motion>", self.mouse_motion)
+        self.canvas.bind("<Button-3>", self.move_object)
 
         self.canvas.pack()
 
 
-        self.statusbar = tk.Frame(self.master, bd=1, relief=tk.RAISED)
+        self.statusbar = tk.Frame(self, bd=1, relief=tk.RAISED)
         self.debug_button = tk.Checkbutton(self.statusbar, text = "Debug", variable = self.debug, \
                  onvalue = True, offvalue = False, command=self.draw_grid)
         self.debug_button.pack(side=tk.LEFT)
         self.statuslabel = tk.Label(self.statusbar, text='Отрезки будут отрисованы методом ЦДА')
         self.statuslabel.pack(side=tk.RIGHT)
         self.statusbar.pack()
+        
+        self.dots_need_bar = tk.Frame(self, bd=1, relief=tk.RAISED)
+        self.dots_need_label = tk.Label(self.dots_need_bar, text='Количество граничных точек: ')
+        self.dots_need_label.pack(side=tk.LEFT)
+        self.dots_need = tk.IntVar(value=2)
+        self.dots_need_field = tk.Entry(self.dots_need_bar)
+        self.dots_need_field['textvariable'] = self.dots_need
+        self.dots_need_field.bind('<Key-Return>', self.on_change)
+        self.dots_need_field.pack(side=tk.RIGHT)
+        self.dots_need_bar.pack()
+        self.dots_need_field.config(state='disable')
+
+    def on_change(self, event) -> None:
+        if self.dots_need.get() < 4:
+            self.dots_need.set(4)
+
+    def move_object(self, event) -> None:
+        if self.obj is None:
+            self.obj = self.canvas.find_overlapping(event.x-5, event.y-5, event.x + 5, event.y + 5)
+            print(self.obj)
+        else:
+            self.obj = None
 
     def mouse_pressed(self, event) -> None:
         x, y = event.x, event.y
-        if self.begin is None:
-            if self.debug.get():
-                self.begin = line.Dot(x = x // 10, y = y // 10)
+        if not self.is_multiple_dots:
+            if self.begin is None:
+                if self.debug.get():
+                    self.begin = line.Dot(x = x // 10, y = y // 10)
+                else:
+                    self.begin = line.Dot(x = x, y = y)
             else:
-                self.begin = line.Dot(x = x, y = y)
+                if self.debug.get():
+                    end = line.Dot(x = x // 10, y = y // 10)
+                else:
+                    end = line.Dot(x = x, y = y)
+                dots = self.draw_method(self.begin, end)
+                for dot in dots:
+                    if self.debug.get():
+                        self.canvas.create_rectangle(dot.x * 10, dot.y * 10, dot.x * 10 + 10, dot.y * 10 + 10, fill=color_intesity[dot.i], tags='dline')
+                        print(dot.x, dot.y, dot.i)
+                        self.update()
+                        time.sleep(0.5)
+                    else:
+                        self.canvas.create_rectangle(dot.x, dot.y, dot.x + 1, dot.y + 1, fill=color_intesity[dot.i], tags='line')
+                if not self.debug.get():
+                    self.lines.append(dots)
+                self.begin = None
         else:
             if self.debug.get():
-                end = line.Dot(x = x // 10, y = y // 10)
+                self.dots.append(line.Dot(x = x // 10, y = y // 10))
             else:
-                end = line.Dot(x = x, y = y)
-            dots = self.draw_method(self.begin, end)
-            for dot in dots:
-                if self.debug.get():
-                    self.canvas.create_rectangle(dot.x * 10, dot.y * 10, dot.x * 10 + 10, dot.y * 10 + 10, fill=color_intesity[dot.i], tags='dline')
-                    print(dot.x, dot.y, dot.i)
-                    self.update()
-                    time.sleep(0.5)
-                else:
-                    self.canvas.create_rectangle(dot.x, dot.y, dot.x + 1, dot.y + 1, fill=color_intesity[dot.i], tags='line')
-            if not self.debug.get():
-                self.lines.append(dots) 
-            self.dots = None    
-            self.begin = None
-            self.line = None
+                self.dots.append(line.Dot(x = x, y = y))
+            self.draw_boudary_points()
+            if len(self.dots) == self.dots_need.get():
+                dots = self.draw_method(self.dots)
+                for i in range(1, len(dots)):
+                    if self.debug.get():
+                        self.canvas.create_rectangle(dots[i-1].x * 10, dots[i-1].y * 10, dots[i].x * 10, dots[i].y * 10, fill=color_intesity[dots[i-1].i], tags='dcurve')
+                        print(dots[i].x, dots[i].y, dots[i].i)
+                        self.update()
+                        time.sleep(0.5)
+                    else:
+                        self.canvas.create_line((dots[i-1].x, dots[i-1].y), (dots[i].x, dots[i].y), fill=color_intesity[dots[i].i], tags='curve')
+                if not self.debug.get():
+                    self.curves.append(dots)
+                self.dots.clear()
+
 
     def mouse_motion(self, event) -> None:
-        if self.begin is not None and not self.debug.get():
+        if self.begin is not None and not self.debug.get() and not self.is_multiple_dots:
             x, y = event.x, event.y
             dots = self.draw_method(self.begin, line.Dot(x = x, y = y))
             self.canvas.delete('line')
             self.draw_lines()
             for dot in dots:
                 self.canvas.create_rectangle(dot.x, dot.y, dot.x + 1, dot.y + 1, fill=color_intesity[dot.i], tags='line')
+        elif self.is_multiple_dots and self.obj is not None:
+            for obj in self.obj:
+                self.canvas.moveto(obj, event.x, event.y)
 
     
     def draw_lines(self):
         for line in self.lines:
             for dot in line:
                 self.canvas.create_rectangle(dot.x, dot.y, dot.x + 1, dot.y + 1, fill=color_intesity[dot.i], tags='line')
+
+    def draw_boudary_points(self):
+        i = 0
+        for dot in self.dots:
+            self.canvas.delete('P' + str(i))
+            if self.debug.get():
+                self.canvas.create_oval(dot.x*10 - 5, dot.y*10 - 5, dot.x*10 + 5, dot.y*10 + 5, outline='black', tags='P' + str(i))
+                self.canvas.create_text(dot.x*10 + 15, dot.y*10,text='P' + str(i), tags = 'P' + str(i))
+            else:
+                self.canvas.create_oval(dot.x - 5, dot.y - 5, dot.x + 5, dot.y + 5, outline='black', tags='P' + str(i))
+                self.canvas.create_text(dot.x + 15, dot.y,text='P' + str(i), tags = 'P' + str(i))
+            i += 1
     
     def exit(self) -> None:
         self.destroy()
 
     def dda(self):
         self.draw_method = line.digital_differential_analyzer
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Отрезки будут отрисованы методом ЦДА')
     
     def bresenham(self):
         self.draw_method = line.bresenham
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Отрезки будут отрисованы методом Брезенхема')
 
     def wu(self):
         self.draw_method = line.wu
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Отрезки будут отрисованы алгоритмом Ву')
 
     def circle(self):
         self.draw_method = second_order_lines.circle
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Будет отрисована окружность')
 
     def ellipse(self):
         self.draw_method = second_order_lines.ellipse
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Будет отрисован эллипс')
 
     def hyperbola(self):
         self.draw_method = second_order_lines.hyperbola
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Будет отрисована гипербола')
 
     def parabola(self):
         self.draw_method = second_order_lines.parabola
+        self.is_multiple_dots = False
+        self.dots_need.set(2)
+        self.dots_need_field.config(state='disable')
         self.statuslabel.config(text='Будет отрисована парабола')
+
+    def hermite(self):
+        self.draw_method = curve.hermite_shape
+        self.is_multiple_dots = True
+        self.dots_need.set(4)
+        self.dots_need_field.config(state='disable')
+        self.statuslabel.config(text='Будет отрисована кривая формой Эрмита')
+
+    def bezier(self):
+        self.draw_method = curve.bezier_shape
+        self.is_multiple_dots = True
+        self.dots_need.set(4)
+        self.dots_need_field.config(state='disable')
+        self.statuslabel.config(text='Будет отрисована кривая формой Безье')
+
+    def b_spline(self):
+        self.draw_method = curve.b_spline
+        self.is_multiple_dots = True
+        self.dots_need.set(10)
+        self.dots_need_field.config(state='normal')
+        self.statuslabel.config(text='Будет отрисована кривая b-сплайн')
 
     def draw_grid(self):
         if self.debug.get():
@@ -203,10 +336,11 @@ class App(tk.Tk):
             for line in range(0, self.height + 1, 10):
                 self.canvas.create_line([(0, line), (self.width, line)], fill='#828282', tags='grid_line_h')
         else:
-            self.canvas.delete('grid_line_h', 'grid_line_w', 'dline')
+            self.canvas.delete('grid_line_h', 'grid_line_w', 'dline', 'dcurve')
 
     def clear(self):
-        self.canvas.delete('grid_line_h', 'grid_line_w', 'line', 'dline')
+        self.canvas.delete('grid_line_h', 'grid_line_w', 'line', 'dline', 'curve', 'dcurve')
         self.lines.clear()
+        self.curves.clear()
         if self.debug.get():
             self.draw_grid()
